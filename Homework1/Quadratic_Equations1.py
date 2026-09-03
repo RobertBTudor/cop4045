432badd296f52077e1d740ec12b736f9e251712e
RobertBTudor <robt007b@gmail.com>
2026-09-02 20:24:42 -0400

    Added the matplotlib to graph the quadratic function and its roots

--- Homework1/Quadratic_Equations1.py
+++ Homework1/Quadratic_Equations1.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 
 #Continuous loop that runs until the break condition (triggered by the user pressing enter key for coeficcient a)
 while True:
@@ -25,5 +26,28 @@ while True:
         x2 = (-b - math.sqrt((b**2) - (4*a*c))) / (2*a) 
         print("Two real solutions: ", x1, " and ", x2)
 
+    #--- Creating the graph ---
+    #Creates the chart using matplotlib. First sort by the amount of roots to choose the domain size (xmin/xmax),
+    #Then creates a graph using 150 points. I customized the graph to show all 150 points and made them less thick (similar to the example)
+    if ((b**2) - (4*a*c)) > 0:
+        xmin = x1 - 5
+        xmax = x2 + 5
 
+    elif ((b**2) - (4*a*c)) == 0:
+        xmin = x1 - 5
+        xmax = x1 + 5
 
+    else:
+        xopt = -b / (2*a)
+        xmin = xopt - 5
+        xmax = xopt + 5
+
+    x = [xmin + i * (xmax - xmin) / 149 for i in range(150)]
+
+    y = []
+    for value in x:
+        y.append(a * value**2 + b * value + c)
+
+    plt.plot(x, y, "o-", markersize = 2)
+    plt.axhline(0)
+    plt.show()
