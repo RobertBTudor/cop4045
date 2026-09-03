19703302db5541b3243c23b0a5a8151d91e7941a
RobertBTudor <robt007b@gmail.com>
2026-09-03 13:09:04 -0400

    Renamed files to proper format and created and solved problem 2 (pythagorean theorm)

---
+++ Homework1/p2_Tudor_Robert.py
@@ -0,0 +1,14 @@
+#Tries all posible combinations for a,b,c from 1 to n (inclusive). If it satisfies the pythagorean theorm it gets appended to the list as a tuple. 
+def find_Pythagorean(n):
+    triples = []
+    
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            for c in range(1, n+1):
+                if a**2 + b**2 == c**2:
+                    if (a,b,c) not in triples:
+                        triples.append((a,b,c))
+    return triples
+
+user_n = int(input("Enter a value n: "))
+print(find_Pythagorean(user_n))
